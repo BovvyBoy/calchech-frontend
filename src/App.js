d684aa56eab56f7ea641dd935a7b48d4a65ebeca
Daniel  Bovington <d.bovington@gmail.com>
2020-04-23 20:42:48 +0100

    testing frontend with backend fetch

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  componentDidMount(){
+    fetch('http://localhost:3000/api/v1/plans')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+
+
   render(){
     return (
       <div className="App">
